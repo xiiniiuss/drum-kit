69fac51202423a4bcc031d34c77e9fe0eaa6a63b
Zine eddine <125315802+xiiniiuss@users.noreply.github.com>
2023-03-25 04:39:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrums; i++) {
+
+buttons = document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+    makeSound(this.innerHTML);
+
+    buttonAnimation(this.innerHTML);
+
+    });
+    
+}
+
+document.addEventListener("keydown", function (event) {
+
+    makeSound(event.key)
+
+    buttonAnimation(event.key);
+
+})
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        default:
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+    }
+}
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100)
+
+}
